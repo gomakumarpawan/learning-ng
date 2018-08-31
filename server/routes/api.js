b61bdd7beb65a0f6459df602e2a6e27f585a101f
pawan <pawan.kumar@policyx.com>
2018-08-31 17:11:36 +0530

    updated code

---
+++ server/routes/api.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+
+// Connect
+const connection = (closure) => {
+    return MongoClient.connect('mongodb://gomakumarpawan:pawan0106!@cluster0-shard-00-00-du7n0.mongodb.net:27017,cluster0-shard-00-01-du7n0.mongodb.net:27017,cluster0-shard-00-02-du7n0.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true', (err, db) => {
+        if (err) return console.log(err);
+
+        closure(db);
+    });
+};
+
+// Error handling
+const sendError = (err, res) => {
+    response.status = 501;
+    response.message = typeof err == 'object' ? err.message : err;
+    res.status(501).json(response);
+};
+
+// Response handling
+let response = {
+    status: 200,
+    data: [],
+    message: null
+};
+
+// Get users
+router.get('/users', (req, res) => {
+    connection((db) => {
+        db.collection('users')
+            .find()
+            .toArray()
+            .then((users) => {
+                response.data = users;
+                res.json(response);
+            })
+            .catch((err) => {
+                sendError(err, res);
+            });
+    });
+});
+
+module.exports = router;
